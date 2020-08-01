b64b6e875288e40769c15f5ddbeecdf42d57e125
madams <mtadams42@gmail.com>
2020-08-01 14:00:17 -0500

    publishing changes

--- gatsby-node.js
+++ gatsby-node.js
@@ -57,42 +57,6 @@ exports.createPages = ({ boundActionCreators, graphql }) => {
 }
 
 
-// exports.createPages = async ({ actions, graphql, reporter }) => {
-// 	const { createPage } = actions
-// 	const blogPostTemplate = require.resolve(`./src/templates/blog-template.js`)
-// 	const result = await graphql(`
-// 	  {
-// 		allMarkdownRemark(
-// 		  sort: { order: DESC, fields: [frontmatter___date] }
-// 		  limit: 1000
-// 		) {
-// 		  edges {
-// 			node {
-// 			  frontmatter {
-// 				slug
-// 			  }
-// 			}
-// 		  }
-// 		}
-// 	  }
-// 	`)
-// 	// Handle errors
-// 	if (result.errors) {
-// 		reporter.panicOnBuild(`Error while running GraphQL query.`)
-// 		return
-// 	}
-// 	result.data.allMarkdownRemark.edges.forEach(({ node }) => {
-// 		createPage({
-// 			path: node.frontmatter.slug,
-// 			component: blogPostTemplate,
-// 			context: {
-// 				// additional data can be passed via context
-// 				slug: node.frontmatter.slug,
-// 			},
-// 		})
-// 	})
-// }
-
 // build to docs instead of public
 
 exports.onPostBuild = () => {
