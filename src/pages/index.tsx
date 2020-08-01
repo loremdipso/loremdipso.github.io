39cfeb6d56057510451b3d3cd88f930978782917
madams <mtadams42@gmail.com>
2020-08-01 12:27:29 -0500

    updating navbar

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,5 @@
 import React, { useState } from "react";
 import "../styles/styles.scss";
-// import { BrowserRouter, Switch, Route, NavLink } from "react-router-dom";
 import { Navbar, Nav, Container } from "react-bootstrap";
 import "../../node_modules/bootstrap/dist/css/bootstrap.css";
 import { APP_ROUTES } from "../config/routes";
@@ -9,75 +8,61 @@ import Blog from "../components/blog";
 import Resume from "../components/resume";
 import { Router, Link, Location } from "@reach/router";
 import { TransitionGroup, CSSTransition } from "react-transition-group";
-import Switch from "react-bootstrap/esm/Switch";
 
 export default function Home() {
 	return (
 		<div>
 			<Navbar sticky="top" bg="light" collapseOnSelect>
 				<Nav>
-					<Nav.Link as={Link} to={APP_ROUTES.home}>
-						About
-					</Nav.Link>
-
-					<Nav.Link as={Link} to={APP_ROUTES.blog}>
-						Blog
-					</Nav.Link>
-
-					<Nav.Link as={Link} to={APP_ROUTES.resume}>
-						Resumé
-					</Nav.Link>
+					<FancyNavLink to={APP_ROUTES.home}>About</FancyNavLink>
+					<FancyNavLink to={APP_ROUTES.blog}>Blog</FancyNavLink>
+					<FancyNavLink to={APP_ROUTES.resume}>Resumé</FancyNavLink>
 				</Nav>
 			</Navbar>
 
-			<div className="scrollable">
-				<Location>
-					{({ location }) => (
-						<Container>
-							<TransitionGroup>
-								<CSSTransition
-									key={location.key}
-									timeout={{ enter: 300, exit: 300 }}
-									classNames={"fade"}
-								>
-									<section className="route-section">
-										<Router location={location}>
-											<Profile path={APP_ROUTES.home} />
-											<Blog path={APP_ROUTES.blog} />
-											<Resume path={APP_ROUTES.resume} />
-										</Router>
-									</section>
-
-									{/* <Link to={APP_ROUTES.resume}>yo</Link>
-					<FadeTransitionRouter>
-						<Profile path={APP_ROUTES.home} />
-						<Blog path={APP_ROUTES.blog} />
-						<Resume path={APP_ROUTES.resume} />
-					</FadeTransitionRouter> */}
-								</CSSTransition>
-							</TransitionGroup>
-						</Container>
-					)}
-				</Location>
-			</div>
+			<FancyRouter>
+				<Profile path={APP_ROUTES.home} />
+				<Blog path={APP_ROUTES.blog} />
+				<Resume path={APP_ROUTES.resume} />
+			</FancyRouter>
 		</div>
 	);
 }
 
-const FadeTransitionRouter = (props) => (
-	<Location>
-		{({ location }) => (
-			<TransitionGroup className="transition-group">
-				<CSSTransition key={location.key} classNames="fade" timeout={500}>
-					{/* the only difference between a router animation and
-				any other animation is that you have to pass the
-				location to the router so the old screen renders
-				the "old location" */}
-					<Router location={location} className="router">
-						{props.children}
-					</Router>
-				</CSSTransition>
-			</TransitionGroup>
-		)}
-	</Location>
-);
+function FancyNavLink(props: { to: string; children: any }) {
+	return (
+		<Link
+			to={props.to}
+			className="nav-link"
+			getProps={({ isCurrent }) => {
+				return { className: isCurrent ? "nav-link active" : "nav-link" };
+			}}
+		>
+			{props.children}
+		</Link>
+	);
+}
+
+function FancyRouter({ children }: { children: any }) {
+	return (
+		<div>
+			<Location>
+				{({ location }) => (
+					<Container>
+						<TransitionGroup>
+							<CSSTransition
+								key={location.key}
+								timeout={{ enter: 300, exit: 300 }}
+								classNames={"fade"}
+							>
+								<section className="route-section">
+									<Router location={location}>{children}</Router>
+								</section>
+							</CSSTransition>
+						</TransitionGroup>
+					</Container>
+				)}
+			</Location>
+		</div>
+	);
+}
