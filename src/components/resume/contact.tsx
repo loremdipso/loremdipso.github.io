835924a32929a66e16ebc9aee02df3db232c1db7
Michael Adams <madams@gmail.com>
2020-08-29 23:44:16 -0500

    Refactoring as we work on V2.

--- src/components/resume/contact.tsx
+++ src/components/resume/contact.tsx
@@ -1,16 +1,19 @@
 import React, { useState, useEffect } from "react";
 import { ISkill, IContact } from "./interfaces";
+import { ResumeVersionContext } from "./resume";
 
-export default function Contact({ data }: { data: IContact }) {
+export function Contact({ data }: { data: IContact }) {
+	const version = React.useContext(ResumeVersionContext);
 	return (
 		<div className="contacts">
-			<Line name="Address">
+			{version === 2 && <div className="header">Personal Info</div>}
+			{/* <Line name="Address">
 				{data.street && <div>{data.street}</div>}
 				<div>
 					{data.city}, {data.state} {data.zip}
 				</div>
-			</Line>
-			<Line name="Email">{data.email && <div>{data.email}</div>}</Line>
+			</Line> */}
+			{data.email && <Line name="Email"><div>{data.email}</div></Line>}
 			<Line name="Mobile">
 				<div>{data.phone}</div>
 			</Line>
@@ -19,45 +22,22 @@ export default function Contact({ data }: { data: IContact }) {
 }
 
 function Line(props: { name: string; children: any }) {
-	return (
-		<>
-			<div>{props.children}</div>
-			<div className="label">{props.name}</div>
-		</>
-	);
-}
+	const version = React.useContext(ResumeVersionContext);
+	switch (version) {
+		case 2:
+			return (
+				<>
+					<div className="label">{props.name}</div>
+					<div>{props.children}</div>
+				</>
+			);
 
-{
-	/* <div class="section contact">
-	<h2 class="section-header">Contact</h2>
-	<div class="content">
-		<div class="main-address">
-			<span class="label">Address</span>
-			<span class="content">
-				250 Elm Street, Apt 3 Cambridge, MA 02139 United States
-			</span>
-		</div>
-
-		<div class="phone-number phone-mobile">
-			<span class="label">Mobile</span>
-			<span class="content">+1 (617) 390-4180</span>
-		</div>
-
-		<div class="email-address">
-			<span class="label">Email</span>
-			<span class="content">
-				<a href="mailto:jon@tsp.io">jon@tsp.io</a>
-			</span>
-		</div>
-
-		<div class="member-url-resource url-portfolio">
-			<span class="label">Portfolio</span>
-			<span class="content">
-				<a href="http://tsp.io" target="_blank">
-					tsp.io
-				</a>
-			</span>
-		</div>
-	</div>
-</div>; */
+		default:
+			return (
+				<>
+					<div>{props.children}</div>
+					<div className="label">{props.name}</div>
+				</>
+			);
+	}
 }
--- src/components/resume/resume.tsx
+++ src/components/resume/resume.tsx
@@ -3,12 +3,26 @@ import { IData, IEducation, ISkill, IExperience } from "./interfaces";
 import Education from "./education";
 import Experiences from "./experience";
 import Skills from "./skills";
-import Contact from "./contact";
+import { Contact } from "./contact";
 import Name from "./name";
 import "./resume.scss";
 
+export const ResumeVersionContext = React.createContext(1);
+
 export default function Resume(props: { path?: string }) {
 	const [data, setData] = useState(null as IData);
+	const [version, setVersion] = useState(1);
+	const [isDebugging, setIsDebugging] = useState(false);
+
+	useEffect(() => {
+		const urlParams = new URLSearchParams(window.location.search);
+		if (urlParams.has("debug")) {
+			setIsDebugging(true);
+		}
+		if (urlParams.has("version")) {
+			setVersion(parseInt(urlParams.get("version")));
+		}
+	}, []);
 
 	useEffect(() => {
 		fetch("/resume.json").then((result) => {
@@ -22,32 +36,74 @@ export default function Resume(props: { path?: string }) {
 		return null;
 	}
 
-	return (
-		<div className="resume">
-			<div className="content">
-				<div>
+	return <ResumeVersionContext.Provider value={version}>
+		{/* TODO: remove this */}
+		{isDebugging &&
+			<button onClick={() => {
+				setVersion(version === 1 ? 2 : 1);
+			}}>Toggle</button>
+		}
+		<ResumeContent data={data} />
+	</ResumeVersionContext.Provider>;
+}
+
+
+function ResumeContent({ data }: { data: IData }) {
+	const version = React.useContext(ResumeVersionContext);
+	switch (version) {
+		case 2:
+			return <div className="resume take2">
+				<div className="content">
+					<div className="header">
+						<Name data={data} />
+					</div>
+
+					<div className="body">
+						<div className="lefter">
+							<div className="section">
+								<div className="header">Experience</div>
+								<Experiences data={data.experiences} />
+							</div>
+
+							<div className="section">
+								<div className="header">Education</div>
+								<Education data={data.educations} />
+							</div>
+						</div>
+
+						<div className="sidebar">
+							<Contact data={data.contact} />
+							{/* <Skills data={data.skills} /> */}
+						</div>
+					</div>
+				</div>
+			</div>;
+
+		default:
+			return <div className="resume take1">
+				<div className="content">
 					<div className="grid">
 						<div>
 							<Name data={data} />
-							{/* <Skills data={data.skills} /> */}
 						</div>
 
 						<div>
 							<Contact data={data.contact} />
 						</div>
 					</div>
-				</div>
 
-				<div className="section">
-					<div className="header">Experience</div>
-					<Experiences data={data.experiences} />
-				</div>
+					{/* <Skills data={data.skills} /> */}
 
-				<div className="section">
-					<div className="header">Education</div>
-					<Education data={data.educations} />
+					<div className="section">
+						<div className="header">Experience</div>
+						<Experiences data={data.experiences} />
+					</div>
+
+					<div className="section">
+						<div className="header">Education</div>
+						<Education data={data.educations} />
+					</div>
 				</div>
-			</div>
-		</div>
-	);
+			</div>;
+	}
 }
