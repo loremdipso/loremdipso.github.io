78cd43904aa490e62d204262e0647f6d1bf7b716
Michael Adams <madams@gmail.com>
2020-10-16 07:30:58 -0600

    Minor visual fixes for mobile

--- src/components/projects/projects.tsx
+++ src/components/projects/projects.tsx
@@ -14,14 +14,14 @@ export default function Projects() {
 				<div className="image-container">
 					<img src="/images/snake.png" className="simple-image small" />
 
-					<span>
+					<div>
 						Simple snake game built using HTML5 canvas and Rust's web assembly
 						support. It was definitely more work than just writing JS by hand,
 						and I doubt it's any faster. That said, it was a very fun project,
 						and I wouldn't mind similar work in future. I can see Rust WASM
 						being useful in another project when you care about correctness, or
 						some large project where you care about maintenance costs.
-					</span>
+					</div>
 				</div>
 			</ContainerWithSidebar.Section>
 		</ContainerWithSidebar>
