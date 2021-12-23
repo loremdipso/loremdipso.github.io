3980b736068c2889d40d00b3468d24524b741a05
madams <mtadams42@gmail.com>
2021-12-23 00:20:08 -0700

    Updated export script

--- export.sh
+++ export.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
+mv ./docs/CNAME .
 rm -rf docs/
 hugo --cleanDestinationDir --minify
 cat ./docs/resume/index.html | wkhtmltopdf --outline-depth 2 --enable-local-file-access --enable-internal-links - ./docs/resume/resume.pdf
-echo "loremdipso.com" > /docs/CNAME
+mv ./CNAME ./docs/CNAME
