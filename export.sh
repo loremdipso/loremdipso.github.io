d646fe321c6748da0b4d75790f277c30451a5e53
madams <mtadams42@gmail.com>
2021-12-23 00:18:49 -0700

    Update export to include CNAME

--- export.sh
+++ export.sh
@@ -3,3 +3,4 @@
 rm -rf docs/
 hugo --cleanDestinationDir --minify
 cat ./docs/resume/index.html | wkhtmltopdf --outline-depth 2 --enable-local-file-access --enable-internal-links - ./docs/resume/resume.pdf
+echo "loremdipso.com" > /docs/CNAME
