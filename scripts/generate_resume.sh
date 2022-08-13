227b923364f4065509bd8ed67d947c1fbdf2bac1
Michael Adams <mtadams42@gmail.com>
2022-08-12 21:36:19 -0600

    Minor resume fixes

--- scripts/generate_resume.sh
+++ scripts/generate_resume.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-./export.sh
+./scripts/export.sh
 cat ./docs/resume/index.html | wkhtmltopdf --outline-depth 2 --enable-local-file-access --enable-internal-links - ./docs/resume/resume.pdf
