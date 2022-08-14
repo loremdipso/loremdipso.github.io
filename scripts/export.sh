edeb5a7ce581d068f0908d029c746fba1bf8ae48
Michael Adams <mtadams42@gmail.com>
2022-08-14 14:05:37 -0600

    Fixed resume generation

--- scripts/export.sh
+++ scripts/export.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 echo "y" | zola build --output-dir docs
-rm ./docs/resume.pdf
 
 cp -f ./CNAME ./docs/CNAME
-cp -f ./static/resume.pdf ./docs/resume/resume.pdf
+cp -f ./static/resume.pdf ./docs/resume.pdf
--- scripts/generate_resume.sh
+++ scripts/generate_resume.sh
@@ -2,4 +2,4 @@
 
 ./scripts/export.sh
 cat ./docs/resume/index.html | wkhtmltopdf --outline-depth 2 --enable-local-file-access --enable-internal-links - ./static/resume.pdf
-cp -f ./static/resume.pdf ./docs/resume/resume.pdf
+cp -f ./static/resume.pdf ./docs/resume.pdf
