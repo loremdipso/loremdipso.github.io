359bcfe9e3d8ddba321d2a4f45a37c24e302f2a5
Michael Adams <mtadams42@gmail.com>
2022-08-17 18:58:35 -0600

    Fixed the scroll and made scripts async by default.

--- docs/js/post-scroll.js
+++ docs/js/post-scroll.js
@@ -1,70 +1,70 @@
-
 /**
  * This tries valiantly to highlight the right section in the TOC. Does it work?
  * Yes. Is it fast? Eh, maybe?
+ * NOTE: this assumes it's loaded async. Otherwise we'd use the DOMContentLoaded event.
  */
-window.addEventListener("DOMContentLoaded", () => {
-    // map of elements to the IDs they represent
-    const elementToAsideElementMap = new Map();
-    const intersecting = new Set();
-    let highlightedHeader = null;
 
-	const observer = new IntersectionObserver((entries) => {
-        // remove all now non-intersecting entries
-        for (const entry of entries) {
-            const header = entry.target;
-            if (entry.isIntersecting) {
-                intersecting.add(header);
-            } else {
-                intersecting.delete(header);
-            }
-        }
 
-        let minClientTop = -1;
-        let topElement = null;
-        for (const element of intersecting) {
-            let top = element.offsetTop;
-            if (!topElement || top < minClientTop) {
-                minClientTop = top;
-                topElement = element;
-            }
-        }
+// map of elements to the IDs they represent
+const elementToAsideElementMap = new Map();
+const intersecting = new Set();
+let highlightedHeader = null;
 
-        // already done - exit
-        let headerToHighlight = elementToAsideElementMap.get(topElement);
-        if (headerToHighlight === highlightedHeader) {
-            return;
-        }
+const observer = new IntersectionObserver((entries) => {
+	// remove all now non-intersecting entries
+	for (const entry of entries) {
+		const header = entry.target;
+		if (entry.isIntersecting) {
+			intersecting.add(header);
+		} else {
+			intersecting.delete(header);
+		}
+	}
 
-        if (highlightedHeader) {
-            highlightedHeader.classList.remove("active");
-            highlightedHeader = null;
-        }
+	let minClientTop = -1;
+	let topElement = null;
+	for (const element of intersecting) {
+		let top = element.offsetTop;
+		if (!topElement || top < minClientTop) {
+			minClientTop = top;
+			topElement = element;
+		}
+	}
 
-        if (headerToHighlight) {
-            headerToHighlight.classList.add("active");
-            highlightedHeader = headerToHighlight;
-        }
-	});
+	// already done - exit
+	let headerToHighlight = elementToAsideElementMap.get(topElement);
+	if (headerToHighlight === highlightedHeader) {
+		return;
+	}
 
-    function getAsideElement(target) {
-        const id = target.getAttribute("id");
-        const asideElement = document.querySelector(
-            `aside li a[href="#${id}"]`
-        );
-        return asideElement;
-    }
+	if (highlightedHeader) {
+		highlightedHeader.classList.remove("active");
+		highlightedHeader = null;
+	}
 
-	// Track all sections that have an `id` applied, but skip the last child since that's the sidenav
-    let currentAsideElement = null;
-	for (const element of document.querySelectorAll(".post-contents > *:not(:last-child)")) {
-        if (element.tagName === "H3") {
-            currentAsideElement = getAsideElement(element);
-        }
-
-        if (currentAsideElement) {
-            elementToAsideElementMap.set(element, currentAsideElement);
-            observer.observe(element);
-        }
+	if (headerToHighlight) {
+		headerToHighlight.classList.add("active");
+		highlightedHeader = headerToHighlight;
 	}
 });
+
+function getAsideElement(target) {
+	const id = target.getAttribute("id");
+	const asideElement = document.querySelector(`aside li a[href="#${id}"]`);
+	return asideElement;
+}
+
+// Track all sections that have an `id` applied, but skip the last child since that's the sidenav
+let currentAsideElement = null;
+for (const element of document.querySelectorAll(
+	".post-contents > *:not(:last-child)"
+)) {
+	if (element.tagName === "H2") {
+		currentAsideElement = getAsideElement(element);
+	}
+
+	if (currentAsideElement) {
+		elementToAsideElementMap.set(element, currentAsideElement);
+		observer.observe(element);
+	}
+}
