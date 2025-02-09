da71776f85daa461b244830145dd491296496802
Michael Adams <mtadams42@gmail.com>
2025-02-09 12:57:32 -0800

    Starting a link blog

---
+++ content/posts/wait-should-i-start-a-linkblog.md
@@ -0,0 +1,20 @@
+---
+title: "Wait, should I start a link-blog too??"
+description: "Jumping on the bandwagon"
+slug: wait-should-i-start-a-linkblog
+date: 2025-02-09
+draft: false
+taxonomies: { tags: ["writing", "mini", "link blog"] }
+---
+
+About a week ago I read [Xuanwo's post](https://xuanwo.io/links/2025/01/link-blog/), which referenced [Simon Wilson's post](https://simonwillison.net/2024/Dec/22/link-blog/), which was then responed to in another of [Simon Wilson's post](https://simonwillison.net/2025/Feb/4/build-a-link-blog/) in a delightful flurry of back-and-forths reminiscent of Civil War era [letters from the front](https://www.youtube.com/watch?v=ODM5WqKLxTA).
+
+tl;dr - you write baby posts based off of your reaction to other people's posts. Like a twitter with only one person posting.
+
+The value proposition for me as a writer is attractive: I can write more, think less, I don't have to worry about being original or doing a big project, and at the end of it all have something to look back on. And let's hope this is actually interesting for anyone who wanders over into my corner of the internet.
+
+In any case I'll give it a go. If it doesn't work out then hopefully only the [Wayback Machine](https://web.archive.org/)[^1] will know my failure.
+
+[^1]: Sidenote, the default result for the Wayback Machine is http, not https, and it doesn't automatically redirect you.[^2]
+
+[^2]: What, you came to the footnotes looking for something interesting? They're _footnotes_. There's a reason they didn't make it into the main draft.
--- scripts/new.rb
+++ scripts/new.rb
@@ -1,79 +1,112 @@
 #!/usr/bin/ruby
-require 'erb'
+# frozen_string_literal: true
 
-def readline(prompt)
+require 'erb'
+require 'readline'
+
+def main(_args)
+  template = generate_template
+  title = custom_readline('Title of content (human readable): ')
+  slug = fake_snakecase(title)
+  description = custom_readline('Description of content: ')
+  tags = get_user_tags
+  with_images = readline_boolean('Will there be images? (y/n): ')
+  date = Time.now.strftime('%Y-%m-%d')
+
+  result = template.result_with_hash(
+    {
+      title: title, slug: slug,
+      description: description,
+      tags: tags, date: date
+    }
+  )
+
+  output_path = get_output_path(slug, with_images)
+  File.open(output_path, 'w') { |f| f.puts(result) }
+  puts 'Done :)'
+  exec("vim \"#{output_path}\"")
+end
+
+def custom_readline(prompt)
   print prompt
   rv = gets
-  exit if rv == nil
-  return rv.strip
+  exit if rv.nil?
+  rv.strip
 end
 
 def fake_snakecase(text)
-  return text
+  text
     .downcase
     .delete('^ a-z0-9')
-    .split(" ")
+    .split(' ')
     .map(&:strip)
-    .join("-")
+    .join('-')
     .gsub(/--/, '-')
 end
 
 def readline_boolean(prompt)
-  while true
-    answer = readline(prompt).downcase
-    if answer == "y"
+  loop do
+    answer = custom_readline(prompt).downcase
+    if answer == 'y'
       return true
-    elsif answer == "n"
+    elsif answer == 'n'
       return false
     else
-      puts "ERROR: invalid input"
+      puts 'ERROR: invalid input'
     end
   end
 end
 
-def get_template()
-  template = ERB.new(
-    File.read(File.join(File.dirname(__FILE__), "_template.md")),
-    #0,
-    #"%<>"
+def generate_template
+  ERB.new(
+    File.read(File.join(File.dirname(__FILE__), '_template.md'))
+    # 0,
+    # "%<>"
   )
-  return template
 end
 
-def main(args)
-  template = get_template()
-  title = readline("Title of content (human readable): ")
-  slug = fake_snakecase(title)
-  description = readline("Description of content: ")
-  tags = readline("Tags (comma-separated): ").split(",").map(&:strip)
-  with_images = readline_boolean("Will there be images? (y/n): ")
-  date = Time.now.strftime("%Y-%m-%d")
+def get_user_tags
+  existent_tags = get_existent_tags
+  comp = proc { |s| existent_tags.grep(/^#{Regexp.escape(s)}/) }
+  Readline.completion_append_character = ','
+  Readline.completion_proc = comp
+  p existent_tags
+  user_tags = []
+  while line = Readline.readline('Tags (comma-separated): ', true)
+    break if line == ''
 
-  result = template.result_with_hash({
-    title: title,
-    slug: slug,
-    description: description,
-    tags: tags,
-    date: date,
-  })
+    line.split(',').map(&:strip).each do |p|
+      user_tags.push(p) if user_tags.index(p).nil?
+      existent_tags.delete(p)
+    end
+    puts ''
+    p user_tags
+  end
+  Readline.completion_proc = nil
+  puts ''
+  user_tags
+end
 
-  output_path = get_output_path(slug, with_images)
-  File.open(output_path, 'w') {|f| f.puts(result) }
-  puts "Done :)"
+def get_existent_tags
+  Dir['docs/tags/*'].keep_if { |f| File.directory?(f) }.map { |e| File.basename(e) }
 end
 
 def get_output_path(slug, use_assets_folder)
   # TODO: deal with non-draft/numeric prefixes. For now just prefix with 'draft'
-  #path = File.join(".", "content", "posts", "draft-%s" % slug)
-  path = File.join(".", "content", "posts", slug)
+  # path = File.join(".", "content", "posts", "draft-%s" % slug)
+  path = File.join('.', 'content', 'posts', slug)
   if use_assets_folder
     Dir.mkdir(path)
-    Dir.mkdir(File.join(path, "assets"))
-    path = File.join(path, "index.md")
+    Dir.mkdir(File.join(path, 'assets'))
+    path = File.join(path, 'index.md')
   else
-    path += ".md"
+    path += '.md'
   end
-  return path
+  path
 end
 
-main(ARGV.dup)
+if $PROGRAM_NAME == __FILE__
+  Dir.chdir(File.join(File.dirname(__FILE__), '..')) do
+    main(ARGV.dup)
+  end
+end
