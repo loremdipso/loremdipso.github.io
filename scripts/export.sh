2611743e05b25c3ed3e2a30ad8a742d29771647d
Michael Adams <mtadams42@gmail.com>
2025-02-09 13:00:37 -0800

    Publishing

--- scripts/export.sh
+++ scripts/export.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-echo "y" | zola build --output-dir docs
+set -e
 
+echo "y" | zola build --output-dir docs
 cp -f ./CNAME ./docs/CNAME
-cp -f ./static/resume.pdf ./docs/resume.pdf
+#cp -f ./static/resume.pdf ./docs/resume.pdf
